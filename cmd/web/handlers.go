e1ab51ab0e6c9dd4cc424bec873434e88bf4e56b
Jose <jose.c.naylor@gmail.com>
2023-11-08 20:35:22 -0500

    renamed home.tmpl to get rid of deliberate error, moved error handling into helper file

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,7 +9,7 @@ import (
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
     if r.URL.Path != "/" {
-        http.NotFound(w, r)
+        app.notFound(w)
         return
     }
 
@@ -20,26 +20,20 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
     }
     ts, err := template.ParseFiles(files...)
     if err != nil {
-        app.logger.Error(err.Error(), "method", r.Method,
-        "uri", r.URL.RequestURI())
-
-        http.Error(w, "Internal Service Error", http.StatusInternalServerError)
+        app.serverError(w, r, err)
         return
     }
 
     err = ts.ExecuteTemplate(w, "base", nil)
     if err != nil {
-        app.logger.Error(err.Error(), "method", r.Method,
-        "uri", r.URL.RequestURI())
-
-        http.Error(w, "Internal Service Error", http.StatusInternalServerError)
+        app.serverError(w, r, err)
     }
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
     id, err := strconv.Atoi(r.URL.Query().Get("id"))
     if err != nil || id < 1 {
-        http.NotFound(w, r)
+        app.notFound(w)
         return
     }
 
@@ -49,7 +43,7 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
     if r.Method != http.MethodPost {
         w.Header().Set("Allow", http.MethodPost)
-        http.Error(w, "Method Not Allowed \n", http.StatusMethodNotAllowed)
+        app.clientError(w, http.StatusMethodNotAllowed)
         return
     }
 
