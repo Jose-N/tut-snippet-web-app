6c999661fca4da07d35065a568ef83faf0496d7b
Jose <jose.c.naylor@gmail.com>
2023-11-18 20:39:05 -0500

    Added insert and get functionality and a custom error

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"database/sql"
+	"errors"
 	"time"
 )
 
@@ -18,12 +19,41 @@ type SnippetModel struct {
 }
 
 func (m *SnippetModel) Insert(title string, content string,
-expires time.Time) (int, error) {
-    return 0, nil
+expires int) (int64, error) {
+    stmt := `INSERT INTO snippets (title, content, created, expires)
+    VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP, INTERVAL ? DAY))`
+
+    result, err := m.DB.Exec(stmt, title, content, expires)
+    if err != nil {
+        return 0, nil
+    }
+
+    id, err := result.LastInsertId()
+    if err != nil {
+        return 0, nil
+    }
+
+    return id, nil
 }
 
 func (m *SnippetModel) Get(id int) (Snippet, error) {
-    return Snippet{}, nil
+    stmt := `SELECT id, title, content, created, expires
+    FROM snippets
+    WHERE expires > UTC_TIMESTAMP() AND id = ?`
+
+    row := m.DB.QueryRow(stmt, id)
+
+    var s Snippet
+    err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+    if err != nil {
+        if errors.Is(err, sql.ErrNoRows) {
+            return Snippet{}, ErrNoRecord 
+        } else {
+            return Snippet{}, err
+        }
+    }
+
+    return s, nil
 }
 
 func (m *SnippetModel) Latest() ([]Snippet, error) {
